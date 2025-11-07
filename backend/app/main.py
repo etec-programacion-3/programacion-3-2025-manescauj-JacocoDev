f5f32459679a1c2088f6b9890db45ffe5c1619c1
JacocoDev <joaquinmanescau07@gmail.com>
2025-11-07 12:06:04 -0300

    6. Configuración del Proyecto Frontend y Página Principal

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 
 app = FastAPI(title="Proyecto Integrador")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
